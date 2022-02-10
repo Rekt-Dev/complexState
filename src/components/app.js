375423168fc87608d6e89616be103dc450af8021
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-02-10 17:22:23 +0200

    Add files via upload

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+function handleChange(event){
+  const newValue = event.target.value
+  setFullName({
+    
+  })
+console.log(`this is fullName ${fullName.fName}`)
+console.log(`this is newValue ${newValue}`)
+}
+
+function handleSubmit(){
+  console.log("submitted bro")
+ event.preventDefault()
+}
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <p>{fullName.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input onChange={handleChange}
+         name="fName"
+         placeholder="First Name" 
+         />
+        <input onChange={handleChange}
+        name="lName" 
+        placeholder="Last Name"/>
+        <input name="email" 
+        placeholder="Email" 
+        />
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
